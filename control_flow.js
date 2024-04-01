71c027dd09eb5338da4a4c4bc147f01ff15472c0
Hesham Mohammed Noaman <140953153+HeshamNoaman@users.noreply.github.com>
2024-04-02 00:49:31 +0300

    complente the practice task of control flow lab

--- control_flow.js
+++ control_flow.js
@@ -50,3 +50,28 @@ switch (userType) {
 
 
 console.log("user category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
+
+
+// Practice task
+let person = "Employee";
+let services;
+
+if (person === "Employee") {
+    services = "full access to Dietary Services";
+}
+else if (person === "Enrolled Member") {
+    services = "full access to Dietary Services and one-on-one interaction with a dietician";
+} 
+else if (person === "Subscriber") {
+    services = "partial access to Dietary Services";
+} 
+else if (person === "Non-Subscriber") {
+    services = "need to enroll or at least subscribe first to avail this facility"
+} 
+
+console.log(services);
